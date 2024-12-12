8e2cdef5b618b4993b54d05b13001e7580b9e434
cjri <christopher.illingworth@glasgow.ac.uk>
2024-12-12 16:43:13 +0000

    Initial commit
    Initial commit of files

---
+++ Calculations/run_calculations14.sh
@@ -0,0 +1,10 @@
+mkdir Intervention14
+cd Intervention14
+rm -r *_*
+cd ../
+cat run_numbers | parallel --jobs 12 'echo {}; mkdir {}_1; cd {}_1 ; /Users/christopher.illingworth/Documents/Coronavirus/Simulation/UDCA_Project/WriteupCI/Code/run_sim_process --sim {} --verb 1 --intervention 14 > Run{}.out ; cd ../'
+cat run_numbers | parallel --jobs 12 'echo {}; mkdir {}_2; cd {}_2 ; /Users/christopher.illingworth/Documents/Coronavirus/Simulation/UDCA_Project/WriteupCI/Code/run_sim_process --sim {} --verb 1 --intervention 14 > Run{}.out ; cd ../'
+cat run_numbers | parallel --jobs 12 'echo {}; mkdir {}_3; cd {}_3 ; /Users/christopher.illingworth/Documents/Coronavirus/Simulation/UDCA_Project/WriteupCI/Code/run_sim_process --sim {} --verb 1 --intervention 14 > Run{}.out ; cd ../'
+cat run_numbers | parallel --jobs 12 'echo {}; mkdir {}_4; cd {}_4 ; /Users/christopher.illingworth/Documents/Coronavirus/Simulation/UDCA_Project/WriteupCI/Code/run_sim_process --sim {} --verb 1 --intervention 14 > Run{}.out ; cd ../'
+cat run_numbers | parallel --jobs 12 'echo {}; mkdir {}_5; cd {}_5 ; /Users/christopher.illingworth/Documents/Coronavirus/Simulation/UDCA_Project/WriteupCI/Code/run_sim_process --sim {} --verb 1 --intervention 14 > Run{}.out ; cd ../'
+mv ?_? ??_? ???_? Intervention14
